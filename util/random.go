98eb1a1db262d8b4852f69c31d0299e274a768b2
Venugopalarao Dasari <venugrda@gmail.com>
2025-02-24 12:32:43 +0530

    first commit

---
+++ util/random.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var rnd *rand.Rand
+
+func init() {
+	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rnd.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for range n {
+		c := alphabet[rnd.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 100000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"INR", "EUR", "USD"}
+	n := len(currencies)
+	return currencies[rnd.Intn(n)]
+}
