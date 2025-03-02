37022e83d8637c366538634713bf3b707f12e9a5
Venugopalarao Dasari <venugrda@gmail.com>
2025-03-02 12:08:56 +0530

    added interface to the Db calls so we can use it for generating mocks

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,21 +7,26 @@ import (
 	"log"
 )
 
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+type SqlStore struct {
 	db *sql.DB
 	*Queries
 }
 
 // NewStore creates new store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SqlStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a given function within the database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SqlStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		log.Println("Error Occurred while calling Begin Transaction", err)
@@ -57,7 +62,7 @@ type TransferTxResult struct {
 }
 
 // TransferTx performs a meoney transfer from one account to another one
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SqlStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
