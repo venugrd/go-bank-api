303c83acaf52c9f35f7f36932728adaebefcafc4
Venugopalarao Dasari <venugrda@gmail.com>
2025-03-03 17:52:09 +0530

    added transfers api

--- api/server.go
+++ api/server.go
@@ -19,6 +19,8 @@ func NewServer(store db.Store) (server *Server) {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccounts)
 
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
 	return
 }
